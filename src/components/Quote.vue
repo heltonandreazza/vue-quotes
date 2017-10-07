d97fc8a7f64c8af69a93b77d5521c8ab291907b5
Helton Andreazza <helton.prg@gmail.com>
2017-10-07 18:07:48 -0300

    crud quotes

---
+++ src/components/Quote.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-sm6 col-md4 col-lg3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quotes']
+}
+</script>
+
+<style>
+.panel-body {
+  font-family: 'Arizonia', cursive;
+  font-size: 14px;
+  color: #6e6e6e;
+}
+
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
